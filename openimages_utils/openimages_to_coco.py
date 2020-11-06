dd2d00f39e33cc1a94e996252c3920280a347361
Alex Avery <aja9675@rit.edu>
2020-11-06 15:35:42 -0500

    Add train/val/test split functionality
    Note that images and annotations remain in their same locations.
    Only the json files are updates to each include their respective
    subset of images.
    
    Signed-off-by: Alex Avery <aja9675@rit.edu>

--- openimages_utils/openimages_to_coco.py
+++ openimages_utils/openimages_to_coco.py
@@ -29,10 +29,15 @@ import json
 import os
 import re
 import fnmatch
+import argparse
+import random
 from PIL import Image
 import numpy as np
 from pycococreatortools import pycococreatortools
+from copy import deepcopy
 
+# Train, val, test split
+DATA_SPLIT = (85,5,10)
 
 INFO = {
     "description": "boxes and chickens",
@@ -83,9 +88,21 @@ def filter_for_annotations(root, files, image_filename):
 
     return files
 
-def oi_to_coco(image_dir, mask_dir, out_json_filename):
+# Split the list of files into train, val, test with DATA_SPLIT percentages
+def train_val_test_split(file_list):
+    num_files = len(file_list)
+    random.shuffle(file_list)
+    train_split = num_files * DATA_SPLIT[0] // 100
+    val_split = train_split + (num_files * DATA_SPLIT[1] // 100)
+    train_imgs, val_imgs, test_imgs = np.split(file_list, (train_split, val_split))
+    return (train_imgs, val_imgs, test_imgs)
 
-    coco_output = {
+def oi_to_coco(args):
+    image_dir = args.image_dir
+    mask_dir = args.mask_dir
+    out_json_filename = args.out_json_filename
+
+    coco_output_common = {
         "info": INFO,
         "licenses": LICENSES,
         "categories": CATEGORIES,
@@ -93,9 +110,16 @@ def oi_to_coco(image_dir, mask_dir, out_json_filename):
         "annotations": []
     }
 
-    image_id = 1
-    segmentation_id = 1
-    
+    # Initialize train, val, test outputs
+    coco_outputs = (deepcopy(coco_output_common), deepcopy(coco_output_common), deepcopy(coco_output_common))
+
+    # Each 'dataset' has it's own counter
+    image_ids = [1, 1, 1]
+    segmentation_ids = [1, 1, 1]
+
+    # Keep track of files with missing annotations
+    missing_annotation_files = []
+
     # Process each category/class
     for category in CATEGORIES:
         class_id = category['id']
@@ -108,58 +132,83 @@ def oi_to_coco(image_dir, mask_dir, out_json_filename):
         for root, _, files in os.walk(current_cat_path):
             image_files = filter_for_jpeg(root, files)
 
-            # go through each image
-            for image_filename in image_files:
-                print("Processing %s" % image_filename)
-                image = Image.open(image_filename)
+            # Generate the random splits
+            train_imgs, val_imgs, test_imgs = train_val_test_split(image_files)
 
-                # filter for associated png annotations
-                # Note that this is SLOW if you don't remove unused annotations
-                for root, _, files in os.walk(os.path.join(mask_dir, class_name)):
-                    annotation_files = filter_for_annotations(root, files, image_filename)
+            # For each subset of images
+            for i, image_files in enumerate([train_imgs, val_imgs, test_imgs]):
+                coco_output = coco_outputs[i]
 
-                    # go through each associated annotation
-                    for annotation_filename in annotation_files:
-                        #print(annotation_filename)
+                # For each jpg image
+                for image_filename in image_files:
+                    print("Processing %s" % image_filename)
+                    image = Image.open(image_filename)
 
-                        category_info = {'id': class_id, 'is_crowd': 'crowd' in image_filename}
-                        binary_mask = np.asarray(Image.open(annotation_filename)
-                            .convert('1')).astype(np.uint8)
-                        
-                        annotation_info = pycococreatortools.create_annotation_info(
-                            segmentation_id, image_id, category_info, binary_mask,
-                            image.size, tolerance=2)
+                    # Filter for associated png annotations
+                    for root, _, files in os.walk(os.path.join(mask_dir, class_name)):
+                        annotation_files = filter_for_annotations(root, files, image_filename)
+                        if len(annotation_files) == 0:
+                            #print("Missing annotation for %s" % image_filename)
+                            missing_annotation_files.append(image_filename)
 
-                        if annotation_info is not None:
-                            coco_output["annotations"].append(annotation_info)
+                        # For each associated annotation
+                        for annotation_filename in annotation_files:
+                            #print(annotation_filename)
 
-                        segmentation_id = segmentation_id + 1
+                            category_info = {'id': class_id, 'is_crowd': 'crowd' in image_filename}
+                            binary_mask = np.asarray(Image.open(annotation_filename)
+                                .convert('1')).astype(np.uint8)
 
-                '''
-                Quirk here - Using oi_download_dataset to download images puts them in
-                a sub dir called images. This is because there's another one next to
-                it with the pascal or darknet annotations (which we're not using)
-                '''
-                image_path = os.path.join(class_name, 'images', os.path.basename(image_filename))
-                image_info = pycococreatortools.create_image_info(image_id, image_path, image.size)
-                coco_output["images"].append(image_info)
+                            # 0 tolerance for maximum accuracy
+                            annotation_info = pycococreatortools.create_annotation_info(
+                                segmentation_ids[i], image_ids[i], category_info, binary_mask,
+                                image.size, tolerance=0)
 
-                image_id = image_id + 1
+                            if annotation_info is not None:
+                                coco_output["annotations"].append(annotation_info)
 
-    with open(out_json_filename, 'w') as output_json_file:
-        print("Writing COCO json file: %s" % output_json_file)
-        json.dump(coco_output, output_json_file)
+                            segmentation_ids[i] += 1
+
+                    '''
+                    Quirk here - Using oi_download_dataset to download images puts them in
+                    a sub dir called images. This is because there's another one next to
+                    it with the pascal or darknet annotations (which we're not using)
+                    '''
+                    image_path = os.path.join(class_name, 'images', os.path.basename(image_filename))
+                    image_info = pycococreatortools.create_image_info(image_ids[i], image_path, image.size)
+                    coco_output["images"].append(image_info)
+
+                    image_ids[i] += 1
+
+    if len(missing_annotation_files) != 0:
+        print("Warning. Missing annotations for %i files: " % len(missing_annotation_files))
+        for f in missing_annotation_files:
+            print(f)
+        print("Warning. This may skew results\n")
+
+    # Remove '.json' if it exists in the output json filename
+    out_json_base = out_json_filename.replace('.json', "")
+    # Create our file names for each of our sets
+    out_jsons = (out_json_base+'_train.json', out_json_base+'_val.json', out_json_base+'_test.json')
+
+    # Write out the json files
+    for i, coco_output in enumerate(coco_outputs):
+        with open(out_jsons[i], 'w') as output_json_file:
+            print("Writing COCO json file: %s" % out_jsons[i])
+            json.dump(coco_output, output_json_file)
 
 
 def main():
-    if len(sys.argv) != 4:
-        sys.exit("Usage: openimages_to_coco.py <image_dir> <mask_dir> <output_json_filename>")
+    parser = argparse.ArgumentParser()
 
-    image_dir = sys.argv[1]
-    mask_dir = sys.argv[2]
-    out_json_filename = sys.argv[3]
+    parser.add_argument('image_dir', metavar='image_dir', type=str, help='Images directory')
+    parser.add_argument('mask_dir', metavar='mask_dir', type=str, help='Masks directory')
+    parser.add_argument('out_json_filename', metavar='out_json_filename', type=str, \
+        help='output json filename (with or without .json)')
+    parser.set_defaults(func=oi_to_coco)
 
-    oi_to_coco(image_dir, mask_dir, out_json_filename)
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == "__main__":
